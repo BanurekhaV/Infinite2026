1f16006ed3bb7f6fb93b14206783912c8b62a872
BanurekhaV <rekhasairam@hotmail.com>
2026-03-23 18:31:16 +0530

    CSharp Basics

---
+++ CSharp/FirstConsole/FirstConsole/BasicClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace FirstConsole
+{    
+        internal class Program
+        {
+             static void Main()
+            {
+                Console.WriteLine("Hello and Welcome to Dotnet..");
+                //Program program = new Program();
+                //program.Show();
+                //Program p2 = new Program();
+                //p2.Show();
+                //Print();
+            //calling static functions/methods of another class
+                Second.Display();
+                Console.Read();
+            }
+
+          static void Print()  // class function that needs no objects
+          {
+            Console.WriteLine("This is print");
+          }
+           public void Show()   // instance function , that need object
+           {
+            Console.WriteLine("This is Show");
+           }
+        }
+    class Second
+
+    {
+
+        public static void Display()
+
+        {
+            Console.Write("Enter your First Name :");
+            string fname = Console.ReadLine();
+            Console.Write("Enter your Last Name :");
+            string lname = Console.ReadLine();
+            Console.WriteLine("Your First Name is : " + fname + "and your Last Name is :" + lname);  // concatenation
+            Console.WriteLine("Your First name is :{0} and your Last Name is {1}", fname,lname); //placeholder
+            Console.WriteLine($"Your Name is :{fname} and your Last name is {lname} ");  // string interpolation
+        }
+    }
+}
---
+++ CSharp/FirstConsole/FirstConsole/TypeConversion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsole
+{
+    internal class TypeConversion
+    {
+        static void Main()
+        {
+            Console.WriteLine("Minimum Value of integer {0} ", int.MinValue);
+            Console.WriteLine("Maximum is {0} ", int.MaxValue);
+
+            //declaring a primitive type
+            int num = 100;
+            Console.WriteLine("num is " +  num);
+            float f = num;    // implicit conversion
+            Console.WriteLine("num is " + num);
+            f = 2456.55f;
+            Console.WriteLine("float is " + f);
+            num = (int)f;    // explicit conversion using typecasting
+            Console.WriteLine("float is " + num);
+            num = Convert.ToInt32(f);  // explicit conversion using function
+            Console.WriteLine("float is " + num);
+            Console.Read();
+        }
+    }
+}
