3a2ff6c493c070b049748e34d78f9bc3907275d1
BanurekhaV <rekhasairam@hotmail.com>
2026-03-24 18:31:19 +0530

    day1 csharp

--- CSharp/Day2/Day2/Program.cs
+++ CSharp/Day2/Day2/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Day2
 {
-    internal class Program
+    internal class Program 
     {
         static void Main()
         {
@@ -14,6 +14,8 @@ namespace Day2
             arrays.SingleDimension();
             Console.WriteLine("-------------------");
             arrays.TwoDimension();
+            Console.WriteLine("-------------------");
+            arrays.JaggedArray();
             Console.Read();
         }
         public void SingleDimension()
@@ -36,21 +38,59 @@ namespace Day2
         }
         public void TwoDimension()
         {
-            int[,] myarray = new int[3, 3] { { 1, 2, 3, }, { 4, 5, 6, },{7,8,9 } };
+            int[,] myarray = new int[3, 3] { { 1, 2, 3, }, { 4, 5, 6, }, { 7, 8, 9 } };
             Console.WriteLine(myarray[1, 1]);
 
             //1 st loop to iterate the rows
-           // for (int i = 0; i < 2; i++)
-           for(int i=0;i<myarray.GetLength(0);i++)
-           {
+            // for (int i = 0; i < 2; i++)
+            for (int i = 0; i < myarray.GetLength(0); i++)
+            {
                 //loop 2 for columns 
-               // for (int j = 0; j < 3; j++)
-               for(int j=0;j<myarray.GetLength(1);j++)
-               {
+                // for (int j = 0; j < 3; j++)
+                for (int j = 0; j < myarray.GetLength(1); j++)
+                {
                     Console.Write(myarray[i, j] + " ");
-               }
+                }
                 Console.WriteLine();
-           }
+            }
+        }
+
+        public void JaggedArray()
+        {
+            //declare a jagged array of 2 rows
+            int[][] myjagg = new int[2][];
+
+            //let us now set the size for each array element
+            myjagg[0] = new int[3];
+            myjagg[1] = new int[2];
+
+            //let u sinitialize the jagged array
+            myjagg[0][0] = 2;
+            myjagg[0][1] = 4;
+            myjagg[0][2] = 6;
+
+            myjagg[1][0] = 1;
+            myjagg[1][1] = 3;
+
+            //2. initializing in another way
+            int[][] jagg2 =
+            {
+                new int[] { 5, 10, 15, 20 },
+                new int[] { 25, 30 },
+                new int[] { 35, 40, 45 }
+            };
+
+            //to display the elements of the above jagged array
+            for (int i = 0; i < jagg2.Length; i++)
+            {
+                Console.WriteLine("Number of elements at Row : " + i + "is : " + jagg2[i].Length);
+                //inner loop
+                for(int j=0; j<jagg2[i].Length; j++)
+                {
+                    Console.Write(jagg2[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
