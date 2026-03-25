25e464a41025091263bcd0c15d368793be4f66a7
BanurekhaV <rekhasairam@hotmail.com>
2026-03-25 16:58:21 +0530

    day2 csharp

--- CSharp/Day2/Day2/ReadOnly_Constants.cs
+++ CSharp/Day2/Day2/ReadOnly_Constants.cs
@@ -11,19 +11,21 @@ namespace Day2
          readonly int myvar1 = 5;  // readonly initialized at the time of declaration
          static int mystat1 = 10;  // static
          int mynonstat1 = 15;   // non static
+         const int myconst = 20; //constant
+         const float PI = 3.14f;
 
         //constructor definition
         public ReadOnly_Constants()
         {
-            myvar1 = 500;
+            myvar1 = 500;           
         }
         public static void UnderstandingReadonlyConstants()
         {
             ReadOnly_Constants rc = new ReadOnly_Constants();
-            Console.WriteLine("The value of myvar 1 is " + rc.myvar1);
+            Console.WriteLine("The value of myvar 1 is " + rc.myvar1 +  " and myconst is : " + myconst);
 
             ReadOnly_Constants rc2 = new ReadOnly_Constants();
-            Console.WriteLine("The value of myvar 1 is " + rc2.myvar1);
+            Console.WriteLine("The value of myvar 1 is " + rc2.myvar1 + " and myconst is : " + myconst);
         }
     }
 }
---
+++ CSharp/Day3/Day3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Employee
+    {
+        int Empid;     //4
+        string EmpName; //10
+        float Empsalary;    //8
+
+        public Employee()  // 1. Empty constructor
+        {
+            Empid = 1;
+            EmpName = "Arun";
+            Empsalary = 43000;
+        }
+
+        internal Employee(int e, string s)  // 2. Parameterized constructor
+        {
+            Empid = e;
+            EmpName = s;
+        }
+        public void ShowEmp()      // 4
+        {
+            Console.WriteLine($"Empid :{Empid}, Name : {EmpName} and Salary :{Empsalary}");
+        }
+
+        ~Employee()
+        {
+            Console.WriteLine("bye from Employee");
+            Console.Read();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.ShowEmp();
+            Employee emp2 = new Employee(2,"Kaniga");
+            emp = null;
+            GC.Collect();
+            emp2.ShowEmp();
+            Console.Read();
+        }
+    }
+}
