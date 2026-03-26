40f49dd64cc1f8021b8e60cd9e295c62a726d7cd
BanurekhaV <rekhasairam@hotmail.com>
2026-03-26 16:27:33 +0530

    Day 4 csharp

---
+++ CSharp/Day4/Day4/Base-DerivedEg.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    class BaseClass
+    {
+        int bcvariable;
+        //public BaseClass()   // empty constructor
+        //{
+        //    Console.WriteLine("This is Base class");
+        //}
+
+        public BaseClass(int a)
+        {
+            bcvariable = a;
+            Console.WriteLine("That was base class data.." + " " + bcvariable);
+        }
+    }
+
+    class SubClass : BaseClass
+    {
+        int scvariable;
+         public SubClass(int b) : base(b)
+         {
+            scvariable = b;
+            Console.WriteLine("This is Sub class" + " "+ scvariable); 
+         }
+
+        public SubClass(int x, int y):base(y)
+        {
+            scvariable = x;
+            Console.WriteLine(scvariable + " subclass data");
+        }
+     }
+    internal class Base_DerivedEg
+    {
+        static void Main()
+        {
+         // BaseClass bc = new BaseClass();
+        //  BaseClass bc2 = new BaseClass(10);
+
+          SubClass sc = new SubClass(50, 20);
+          Console.Read();
+        }
+    }
+}
---
+++ CSharp/Day4/Day4/OverloadingEg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    internal class OverloadingEg
+    {
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public float add(int x, float y)
+        { 
+            return x + y;
+        }
+
+        public static void swap(int number1, int number2)
+        {
+            number1 = number1 + number2;
+            number2 = number1 - number2;
+            number1 = number1 - number2;
+            Console.WriteLine("Swapping 2 integers number 1 = " + number1 + " number 2 =" + number2);
+        }
+
+        public static void swap(char ch1, char ch2)
+        {
+            char temp = ' ';
+            temp = ch1;
+            ch1 = ch2;
+            ch2 = temp;
+            Console.WriteLine("Swapping 2 Characters ch1 = "+ ch1 + " " + "ch2 = "+ ch2);
+        }
+    }
+
+    class TestOverload
+    {
+       static void Main()
+        {
+            OverloadingEg overloadeg = new OverloadingEg();
+            overloadeg.add(5, 5);
+            overloadeg.add(10, 15.55f);
+            OverloadingEg.swap('a', 'z');
+            OverloadingEg.swap(5, 10);
+            Console.Read();
+        }
+    }
+}
