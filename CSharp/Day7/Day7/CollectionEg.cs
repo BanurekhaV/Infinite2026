ea03481bf1d3b569e4db4e5afa92d6453ee30076
BanurekhaV <rekhasairam@hotmail.com>
2026-03-31 17:08:14 +0530

    Day 7 csharp

---
+++ CSharp/Day7/Day7/CollectionEg.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    internal class CollectionEg
+    {
+        static void Main()
+        {
+            // ArrayListEg();
+            HashTableEg();
+            Console.Read();
+        }
+
+        public static void ArrayListEg()
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1); 
+            arrayList.Add("AAA");
+            arrayList.Add('Z');
+            arrayList.Add(true);
+            arrayList.Add(456.745f);
+            arrayList.Add("Rama");
+            arrayList.Add(125000.00);
+            arrayList.Add('A');
+            //arrayList.Add(false);
+
+            //to insert an element at a particular index
+            arrayList.Insert(3, "Seetha");
+
+            foreach (var v in arrayList)
+            {
+                Console.WriteLine(v);
+            }
+            //to remove a value from the collection
+            arrayList.Remove('A');
+
+            arrayList.RemoveAt(4);  // removing an element at an index location
+
+            foreach (var v in arrayList)
+            {
+                Console.WriteLine(v);
+            }
+            Console.WriteLine($"Count : {arrayList.Count} and Capacity : {arrayList.Capacity}" );
+
+            //insert range
+            ArrayList al2 = new ArrayList();
+            al2.Add(12);
+            al2.Add(4);
+            al2.Add(6);
+            al2.Add(18);
+
+            arrayList.InsertRange(0, al2);
+            foreach (var v in arrayList)
+            {
+                Console.WriteLine(v);
+            }
+
+            //issues with arraylist of different data types
+            //arrayList.Sort();
+            al2.Sort();
+
+            foreach (var v in al2)
+            {
+                Console.WriteLine(v);
+            }
+        }
+
+        public static void HashTableEg()
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("E005", "Praveen");
+            ht.Add("E001", "Arun");
+            ht.Add("E006", "Kishore");
+            ht.Add("E002", "Arun");
+            ht.Add("E008", null);
+
+            //1. iterating using keys
+            foreach (var item in ht.Keys)
+            {
+                Console.WriteLine(item);
+            }
+
+            //2. iterating using values
+            Console.WriteLine("-------------------");
+            foreach (var item in ht.Values)
+            {
+                Console.WriteLine(item);
+            }
+
+            //3. iterating the whole object, using DictionaryEntry
+            foreach(DictionaryEntry de in ht)
+            {
+                Console.Write(de.Key + " " );
+                Console.Write(de.Value);
+                Console.WriteLine();
+            }
+
+            //hashtables are used to store large records,
+            // retrieving by searching is very good with hashtable
+
+            Console.WriteLine("Enter a Key to search :");
+            string ekey = Console.ReadLine();
+
+            if(ht.ContainsKey(ekey))
+            {
+                Console.WriteLine(ekey  + "is = " + ht[ekey] );
+            }
+            else
+                Console.WriteLine("Employee with Key does not exists");
+        }
+        public static void SortedList()
+        {
+            SortedList sl = new SortedList();
+        }
+    }
+}
---
+++ CSharp/Day7/Day7/GradingSystem.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    internal class GradingSystem
+    {
+        //variable for understanding checked and unchecked
+        static int maxInt = 2147483647;
+        public static string GradeCalculator(int score)
+        {
+            if (score > 0)
+            {
+                if (score >= 90 && score <= 100)
+                {
+                    return "A";
+                }
+                else if (score <= 89 && score >= 70)
+                {
+                    return "B";
+                }
+                else
+                {
+                    return "C";
+                }
+            }
+            else
+            {
+                throw new ScoreException("Score has to be greater than 0");                
+            }
+        }
+
+        //example for checked and unchecked exceptions
+       
+        //1. checked expression
+
+        static int CheckOverflow()
+        {
+            int x = 0;
+            try
+            {
+                //the following line raises an exception
+                x = checked(maxInt + 10);
+            }
+            catch (OverflowException oe)
+            {
+                Console.WriteLine("CHECKED : " + oe.ToString());
+            }
+            return x;  // x is 0
+        }
+
+        static int UnCheckOverflow()
+        {
+            int x = 0;
+            try
+            {
+                //the following line avoids an exception
+                x = maxInt + 10;
+            }
+            catch (OverflowException oe)
+            {
+                Console.WriteLine("UNCHECKED : " + oe.ToString());
+            }
+            return x;  // overflow is supressed, and hence x will be -2147483639
+        }
+        static void Main()
+        {
+            try
+            {
+                //GradingSystem gs = new GradingSystem();
+                //Console.WriteLine("Enter your Score :");
+                //int score = Convert.ToInt32(Console.ReadLine());
+                //string grade = GradeCalculator(score);
+                //Console.WriteLine("Your Grade is : {0}", grade);
+            }
+            catch(ScoreException se) 
+            {
+                 Console.WriteLine(se.Message);
+            }
+            Console.WriteLine("----------Checked and Unchecked------------");
+            Console.WriteLine("\n Checked Output :{0} ", CheckOverflow());
+            Console.WriteLine("\n UnChecked Output :{0} ", UnCheckOverflow());
+            Console.Read();
+        }
+    }
+
+    class ScoreException : ApplicationException
+    {
+        public ScoreException(string message) : base(message) { }
+    }
+}
