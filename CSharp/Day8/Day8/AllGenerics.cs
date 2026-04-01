5967045b01c2726786ad80dfedb902425fe11087
BanurekhaV <rekhasairam@hotmail.com>
2026-04-01 13:34:16 +0530

    day 8 csharp

---
+++ CSharp/Day8/Day8/AllGenerics.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8
+{
+    internal class AllGenerics<T> 
+    {
+        private T genericField;
+
+        public T GenericProperty {  get; set; }
+
+        public AllGenerics(T value)
+        {
+            this.genericField = value;
+        }
+
+        public T genericMethod(T genericParameter)
+        {
+            Console.WriteLine("Parameter Type : {0} and the value is {1}", typeof(T).ToString(),genericParameter);
+            Console.WriteLine("Return Type : {0} and the values is {1}", typeof(T).ToString(),genericField);
+            return genericField;
+        }
+    }
+    class Tester
+    {
+        static void Main()
+        {
+            AllGenerics<int> intallgen = new AllGenerics<int>(10);
+            Console.WriteLine(intallgen.genericMethod(100));
+            intallgen.GenericProperty = 50; 
+            Console.WriteLine(intallgen.GenericProperty);
+            Console.WriteLine("++++++++++++++++++++++");
+
+            AllGenerics<string> strallgen = new AllGenerics<string>("Hi Generics");
+            Console.WriteLine(strallgen.genericMethod("String Parameter"));
+
+            strallgen.GenericProperty = "String property";
+            Console.WriteLine(strallgen.GenericProperty);
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Day8/Day8/Generics1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8
+{
+    internal class Generics1<T>
+    {
+        //generic fields
+        private T data;
+
+        //generic property for the field
+        public T Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+
+        //generic method
+        public void Display<T>(string msg, T var1)
+        {
+            Console.WriteLine("{0} : {1}", msg, var1);
+        }
+    }
+
+    class TestGeneric
+    {
+        static void Main()
+        {
+            Generics1<string> refObj = new Generics1<string>();
+            refObj.Data = "Infinite Ltd.";
+            Console.WriteLine(refObj.Data);
+          
+            refObj.Display<int>("Integer", 15);
+            refObj.Display<string>("String", "Hello World");
+            refObj.Display<Char>("Character", 'A');
+            Console.WriteLine("-----------------");
+
+            Generics1<float> genfloat = new Generics1<float>();
+            genfloat.Data = 5.5f;
+            Console.WriteLine(genfloat.Data);
+            Console.Read();
+        }
+    }
+}
