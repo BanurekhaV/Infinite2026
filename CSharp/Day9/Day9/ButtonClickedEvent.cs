db6acce61aa93ac60be072cb074e7f6c6ca30c5e
BanurekhaV <rekhasairam@hotmail.com>
2026-04-03 11:17:07 +0530

    Events

---
+++ CSharp/Day9/Day9/ButtonClickedEvent.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9
+{
+    //1. let us create a custom event Arguments
+   public class ButtonClickedEvent : EventArgs
+    {
+        public string ClickedBy { get; } // read only property
+
+       public ButtonClickedEvent(string clickedBy)
+       {
+            this.ClickedBy = clickedBy;
+       }
+    }
+
+    //2. create a publisher class
+    public class Button
+    {
+        //create an event
+        public event EventHandler<ButtonClickedEvent> ButtonClicked;
+
+        //3. Method to stimulate button click
+        public void Click(string user)
+        {
+            Console.WriteLine("Event Raised");
+            OnButtonClicked(new ButtonClickedEvent(user));
+        }
+        //4. Raise an Event
+        protected virtual void OnButtonClicked(ButtonClickedEvent e)
+        {
+            ButtonClicked.Invoke(this, e);
+        }
+    }
+
+    class Subscriber1
+    {
+        //5. event handler
+        public static void Button_ButtonClicked(object sender, ButtonClickedEvent e)
+        {
+            Console.WriteLine($"Button was Clicked by : {e.ClickedBy}");
+        }
+
+        //6. test the event
+        static void Main()
+        {
+            Button button = new Button();
+
+            //7. to subscribe to the event
+            button.ButtonClicked += Button_ButtonClicked;
+
+            button.Click("Infinite");
+            button.Click("Banurekha");
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Day9/Day9/PatientRecoveredEvent.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9
+{
+    public class PatientRecoveredEvent : EventArgs
+    {
+        public string PatientName { get; set; }
+    }
+
+    //publisher class
+    public class PatientRecoveryPublisher
+    {
+        public event EventHandler<PatientRecoveredEvent> PatientRecovered;
+
+        protected virtual void OnPatientRecovered(string patientname)
+        {
+            PatientRecovered?.Invoke(this, new PatientRecoveredEvent
+            {
+                PatientName = patientname
+            });
+        }
+
+        public void TriggerRecovery(string patientname)
+        {
+            {
+                OnPatientRecovered(patientname);
+            }
+        }
+    }
+
+    //3. subscriber class (that provides methods that match the signature of the delegate)
+    public class Doctor
+    {
+        public void OnPatientRecovered(object sender, PatientRecoveredEvent e)
+        {
+            Console.WriteLine($"Doctor notified : Patient :{e.PatientName} has Recovered ");
+        }
+    }
+
+    public class Nurse
+    {
+        public void OnPatientRecovered(object sender, PatientRecoveredEvent e)
+        {
+            Console.WriteLine($"Nurse notified : Patient :{e.PatientName} has Recovered ");
+        }
+    }
+
+    //create console that connects the publisher and subscribers
+    class HMS
+    {
+        static void Main()
+        {
+            var publisher = new PatientRecoveryPublisher();
+            var doctor = new Doctor();
+            var nurse = new Nurse();
+
+            publisher.PatientRecovered += doctor.OnPatientRecovered;
+            publisher.PatientRecovered += nurse.OnPatientRecovered;
+
+
+            Console.WriteLine("enter Patient name :");
+            string pname = Console.ReadLine();
+
+            publisher.TriggerRecovery(pname);
+            Console.Read();
+        }
+    }
+}
