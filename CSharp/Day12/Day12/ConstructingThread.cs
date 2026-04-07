9200b093c1b5bfa72e22d841296fbf6de266eb6a
BanurekhaV <rekhasairam@hotmail.com>
2026-04-07 13:42:49 +0530

    Day12

---
+++ CSharp/Day12/Day12/ConstructingThread.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Day12
+{
+    internal class ConstructingThread
+    {
+        static void Main()
+        {
+           //1.
+                Thread t1 = new Thread(DisplayNumbers); //internally invokes a threadstart delegate 
+
+            //2.
+            Console.WriteLine("-------using threads--------");
+            t1.Start();
+            Thread.Sleep(1000);
+            Console.WriteLine("---------------using thread delegate-----------");
+            // 2.1 create a threadstart object and associate it with a function name of matching signature
+            ThreadStart obj = new ThreadStart(DisplayNumbers);
+
+            //we will pass threadstartdelegate object as an argument to the thread constructor
+           //2.2
+           Thread t2 = new Thread(obj);
+           
+            //2.3
+            t2.Start();
+            Console.Read();
+        }
+
+        static void DisplayNumbers()
+        {
+            for(int i=0; i<5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ CSharp/Day12/Day12/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Day12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // SimpleThread();
+            //SingleThreads.Method1();
+            //SingleThreads.Method2();
+            //SingleThreads.Method3();
+            Console.WriteLine("----------Working with MultiThreads-----------");
+            Console.WriteLine("Main Thread Started...");
+            Thread t1 = new Thread(MultiThreads.Method1) { Name = "Thread 1" };
+            Thread t2 = new Thread(MultiThreads.Method2) { Name = "Thread 2" };
+            Thread t3 = new Thread(MultiThreads.Method3) { Name = "Thread 3" };
+
+            //executing the methods
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            Console.WriteLine("Main Thread Concluded..");
+
+            Console.Read();
+        }
+
+        static void SimpleThread()
+        {
+            Thread t = Thread.CurrentThread;
+            t.Name = "Test";
+            Console.WriteLine("Current Executing Thread : " + t.Name);
+            Console.WriteLine("Current Executing Thread : " + Thread.CurrentThread.Name);
+        }
+
+        class SingleThreads
+        {
+            //drawbacks of single threaded application
+           public static void Method1()
+           {
+                for(int i=0; i<=5; i++)
+                {
+                    Console.WriteLine("Method 1 : "+ i);
+                }
+           }
+            public static void Method2()
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine("Method 2 : " + i);
+                    if(i==3)
+                    {
+                        Console.WriteLine("Starting to perform database activity..");
+                        //make the thread sleep for 10 second
+                        Thread.Sleep(10000);
+                        Console.WriteLine("Database activity completed....");
+                    }
+                }
+            }
+            public static void Method3()
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine("Method 3 : " + i);
+                }
+            }
+        }
+
+        class MultiThreads
+        {
+            //drawbacks of single threaded application
+            public static void Method1()
+            {
+                Console.WriteLine("Method 1 started using " + Thread.CurrentThread.Name);
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine("Method 1 : " + i);
+                }
+                Console.WriteLine("Method 1 ended using " + Thread.CurrentThread.Name);
+            }
+            public static void Method2()
+            {
+                Console.WriteLine("Method 2 started using : "+ Thread.CurrentThread.Name);
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine("Method 2 : " + i);
+                    if (i == 3)
+                    {
+                        Console.WriteLine("Starting to perform database activity..");
+                        //make the thread sleep for 10 second
+                        Thread.Sleep(10000);
+                        Console.WriteLine("Database activity completed....");
+                    }
+                }
+                Console.WriteLine("Method 2 ended using " + Thread.CurrentThread.Name);
+            }
+            public static void Method3()
+            {
+                Console.WriteLine("Method 3 started using " + Thread.CurrentThread.Name);
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine("Method 3 : " + i);
+                }
+                Console.WriteLine("Method 3 ended using " + Thread.CurrentThread.Name);
+            }
+        }
+    }
+}
