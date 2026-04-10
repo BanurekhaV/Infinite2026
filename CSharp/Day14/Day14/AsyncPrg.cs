c9454a0d2341a001e35ebc780aaa662312fc9d5c
BanurekhaV <rekhasairam@hotmail.com>
2026-04-10 11:36:27 +0530

    day14 n 15

---
+++ CSharp/Day14/Day14/AsyncPrg.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14
+{
+    internal class AsyncPrg
+    {
+        static void Main()
+        {
+            //Method1();
+            //Method2();
+            Console.WriteLine("-------Await in catch and Finally------");
+            ExceptionAwait.addAsync();
+            Console.WriteLine("-----2nd Example-------");
+            FileRead fr = new FileRead();
+            fr.filereadoperation();
+            Console.Read();
+        }
+
+        public static async Task Method1()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    Console.WriteLine("Method 1 " + i);
+                    //dosomething
+                    Task.Delay(100).Wait();
+                }
+            });
+        }
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine("Method 2 " + i);
+                //dosomething
+                Task.Delay(100).Wait();
+            }
+        }
+
+    }
+
+    class ExceptionAwait
+    {
+        public async static Task addAsync()
+        {
+            try
+            {
+                int[] arr = new int[5];
+                arr[10] = 2;
+            }
+            catch (Exception ex)
+            {
+                //using await in catch
+                 await ExceptionOccured();
+
+                Console.WriteLine("Correct your Code");
+            }
+            finally
+            {
+                await ReleasingResources();
+            }
+        
+        }
+
+        async static Task ExceptionOccured()
+        {
+            Console.WriteLine("Array Exception Occurred");
+        }
+
+        async static Task ReleasingResources()
+        {
+            Console.WriteLine("All occupied resources have been released..");
+        }
+    }
+
+    class FileRead
+    {
+        static string filename;
+        public async void filereadoperation()
+        {
+            try
+            {
+                StreamReader sr = File.OpenText("data.txt");
+                Console.WriteLine("The data is : " + sr.ReadLine());
+                sr.Close();
+            }
+            catch { await FileNotFound(); }
+            finally { await Exitprogram(); }
+        }
+
+        async Task FileNotFound()
+        {
+            Console.WriteLine($"The file {filename}you are trying to read is not found.. try later");
+        }
+
+        async Task Exitprogram()
+        {
+            Console.WriteLine("\n press any key to exit");
+        }
+    }
+}
---
+++ CSharp/Day15/Day15/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15
+{
+    class Employee
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; } = "Janani";
+        public double Salary { get; protected set; } = 30000;
+    }
+    internal class Program
+    {
+        int[] arr = new int[5];   
+
+
+        int ShowOp(int[] x)
+        {
+            x[5] = 10;
+            return x[5];
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            try
+            {
+                p.ShowOp(p.arr);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The method that caused exception : " + nameof(p.ShowOp));
+                Console.WriteLine(ex.Source + ex.StackTrace);
+            }
+                
+            Dictionary<string, string> dic = new Dictionary<string, string>()
+            {
+                {"Emp101", "John" },
+                 {"Emp102", "Joe" },
+                  {"Emp103", "Jane" },
+            };
+
+            //with C# 6.0
+            Dictionary<string, string> dic2 = new Dictionary<string, string>()
+            {
+                ["Emp101"] = "John",
+                ["Emp102"] = "Joe",
+                ["Emp102"] = "Jane",
+            };
+            Console.Read();
+        }
+    }
+}
