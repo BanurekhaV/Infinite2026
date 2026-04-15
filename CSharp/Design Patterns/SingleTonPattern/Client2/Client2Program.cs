221a2a7adb93a13f90a6a337e66917fb1593a0f0
BanurekhaV <rekhasairam@hotmail.com>
2026-04-15 18:30:15 +0530

    Design Patterns

---
+++ CSharp/Design Patterns/SingleTonPattern/Client2/Client2Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SingleTonPattern;
+
+namespace Client2
+{
+    internal class Client2Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton training = Singleton.GetInstance();
+            training.PrintDetails("This is Dot Net Training Going on..");
+            Console.Read();
+        }
+    }
+}
--- CSharp/Design Patterns/SingleTonPattern/SingleTonPattern2/Program.cs
+++ CSharp/Design Patterns/SingleTonPattern/SingleTonPattern2/Program.cs
@@ -10,6 +10,32 @@ namespace SingleTonPattern2
     {
         static void Main(string[] args)
         {
+            //get a singleton instance
+            SingletonCache scache = SingletonCache.GetInstance();
+
+            //adding keys and values to the concurrent dictionary
+            Console.WriteLine("Adding Keys and Values to the Cache");
+            Console.WriteLine($"Adding Id to the Cache {scache.Add("Id",1001)}");
+            Console.WriteLine($"Adding Name to the Cache {scache.Add("Name","Sukanya")}");
+
+            Console.WriteLine($"Adding same Id to the Cache using Add {scache.Add("Id", 2001)}");
+
+            Console.WriteLine($"Adding same Id to the Cache using Addorupdate {scache.AddorUpdate("Id", 2001)}");
+
+            Console.WriteLine($"Fetching values from the Cache :");
+            Console.WriteLine($"Fetching Id from cache : {scache.Get("Id")}");
+
+            Console.WriteLine($"Fetching Name from cache : {scache.Get("Name")}");
+
+            Console.WriteLine($"Remove an Id : {scache.Remove("Id")}");
+
+            Console.WriteLine($"Trying to access removed Key : {scache.Get("Id")}");
+
+            
+            Console.WriteLine($"Fetching Name from cache : {scache.Get("Name")}");
+            scache.Clear();
+            Console.Read();
         }
+
     }
 }
