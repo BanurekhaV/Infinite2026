b9011555a083be68e08facfcc064863b88c4bf9b
BanurekhaV <rekhasairam@hotmail.com>
2026-04-17 11:32:18 +0530

    Testing

---
+++ Testing/MainPrj_NUnit/MainPrj_NUnit/Accounts.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainPrj_NUnit
+{
+    public class Accounts
+    {
+        private string AccountNo;
+        private float Balance;
+        public List<Accounts> beneficiaries;
+
+
+        public Accounts(string accno)
+        {
+            this.AccountNo = accno;
+            this.Balance = 500;
+            beneficiaries = new List<Accounts>();
+        }
+
+        public float CheckBalance()
+        {
+            return Balance;
+        }
+
+        public void Deposit(float amt)
+        {
+            Balance += amt;
+        }
+
+        public void Withdraw(float amt)
+        {
+            if (Balance >= amt)
+                Balance -= amt;
+
+            else
+                throw new Exception("Mot Enough Funds to withdraw..");
+        }
+    }
+}
---
+++ Testing/MainPrj_NUnit/TestAccounts/TestingAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MainPrj_NUnit;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace TestAccounts
+{
+    [TestFixture]
+    public class TestingAccount
+    {
+        //arrange
+        MainPrj_NUnit.Accounts acctobj;
+
+        [SetUp]
+        public void TestingSetUp()
+        {
+            acctobj = new Accounts("12345");
+        }
+
+        [Test]
+        //to test depositmethod
+        public void TestingDepositfor_correctBalance()
+        {
+             //act
+            acctobj.Deposit(1000);
+
+            ClassicAssert.AreEqual(1500, acctobj.CheckBalance());
+        }
+
+        public void TestWithdraw()
+        {
+            acctobj.Withdraw(2000);
+        }
+
+        [Test]
+        public void TestWithdrawThrowsException()
+        {
+            Assert.Throws<Exception>(TestWithdraw);
+        }
+    }
+}
