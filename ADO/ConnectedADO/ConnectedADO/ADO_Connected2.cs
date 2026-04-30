de8934b784a5cfbe7a8011b52565e60a5ac02e1b
BanurekhaV <rekhasairam@hotmail.com>
2026-04-30 13:41:35 +0530

    ado 2

--- ADO/ConnectedADO/ConnectedADO/ADO_Connected2.cs
+++ ADO/ConnectedADO/ConnectedADO/ADO_Connected2.cs
@@ -51,6 +51,11 @@ namespace ConnectedADO
         {
             return access.CustomerOrders(custid);
         }
+
+        public void GetCountOfEmployees(int did)
+        {
+            access.GetEmpCount(did);
+        }
     }
 
     //Data Layer
@@ -145,7 +150,7 @@ namespace ConnectedADO
             return _reader;
         }
 
-        //calling procedures with input parameter
+        //1. calling procedures with input parameter
         public SqlDataReader CustomerOrders(string cid)
         {
             _conn = getDBConnection();
@@ -158,22 +163,44 @@ namespace ConnectedADO
             //_reader = _command.ExecuteReader();
             //return _reader;
 
-            //option 2
-           // _command.Parameters.Add("@customerid",SqlDbType.VarChar).Direction = ParameterDirection.Input;
-           
-            //option 3 using Sqlparameter class object
+
+            //option 2 using Sqlparameter class object
 
             SqlParameter param1 = new SqlParameter();
             param1.ParameterName = "@customerid";
-            param1.Value = cid;
-            param1.DbType = DbType.String;
-            param1.Direction = ParameterDirection.Input;
+                param1.Value = cid;
+                param1.DbType = DbType.String;
+                param1.Direction = ParameterDirection.Input; 
 
             _command.Parameters.Add(param1);
             _reader = _command.ExecuteReader();
             return _reader;
         }
 
+        //calling procedure with input, output, and return values
+        public void GetEmpCount(int deptid)
+        {
+            _conn = new SqlConnection("Data Source=laptop-tjj7d977; initial catalog=infinitedb; trusted_connection=true;");
+            _conn.Open();
+
+            _command = new SqlCommand("sp_getempcount", _conn);
+            _command.CommandType =CommandType.StoredProcedure;
+
+            _command.Parameters.AddWithValue("@deptid", deptid);
+
+            _command.Parameters.Add("@totsal",SqlDbType.Decimal).Direction = ParameterDirection.Output;
+            _command.Parameters.Add("@count", SqlDbType.Int).Direction = ParameterDirection.ReturnValue;
+
+            _command.ExecuteNonQuery();
+
+            //trying to retrieve the output and return values
+            int retcount = (int)_command.Parameters["@count"].Value;
+            decimal outsal = (decimal)_command.Parameters["@totsal"].Value;
+
+            // Console.WriteLine("Total salary :" + );
+            Console.WriteLine($"For the Dept : {deptid} , Total Salary is : {outsal} and the count of Employess : {retcount}");
+
+        }
     }
 
     //client
@@ -218,6 +245,11 @@ namespace ConnectedADO
             {
                 Console.WriteLine(dr[0] + " " + dr[1] + " " + dr[2]+ " " + dr[3]);
             }
+
+            Console.WriteLine("--------Procedure with all Parameters---------");
+            Console.WriteLine("Enter the department id for the details :");
+            int deptid = Convert.ToInt32(Console.ReadLine());
+            region.GetCountOfEmployees(deptid);
             Console.Read();
         }
     }
---
+++ ADO/Disconnected_ADO/Disconnected_ADO/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disconnected_ADO
+{
+    internal class Program
+    {
+        public static SqlConnection con = null;
+        public static SqlDataAdapter adapter = null;
+        public static DataSet ds = null;
+
+        static void Main(string[] args)
+        {
+            DisconnectedDataRead();
+            Console.Read();
+        }
+
+        static void DisconnectedDataRead()
+        {
+            con = new SqlConnection("Data Source = laptop-tjj7d977; initial catalog = Northwind; integrated security =true;");
+           con.Open();
+
+            adapter = new SqlDataAdapter("select * from Region",con);
+
+            ds = new DataSet();
+
+            adapter.Fill(ds,"NRegion");
+
+            DataTable dataTable = ds.Tables["NRegion"];  // dataTable object points to the specified table contents of the DataSet
+
+            //to access the data from the dataset via the datatable we will iterate
+
+            foreach(DataRow row in dataTable.Rows )
+            {
+                foreach(DataColumn dc in dataTable.Columns )
+                {
+                    Console.Write(row[dc] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
