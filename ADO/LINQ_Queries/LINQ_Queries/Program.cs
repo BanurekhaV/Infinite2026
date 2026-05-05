7ce0e67feffb5d061f833158b747e6a9d04f3876
BanurekhaV <rekhasairam@hotmail.com>
2026-05-05 11:38:50 +0530

    Ado 4

---
+++ ADO/LINQ_Queries/LINQ_Queries/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Queries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aggregates_General();
+            //Seed_Aggregations();
+            //Element_At();
+            //First_Operators();
+            Single_Ops();
+            Console.Read();
+        }
+
+        static void Aggregates_General()
+        {
+            int[] numbers = { 2, 34, 5, 6, 7, 8, 9 };
+            var sum = numbers.Sum();
+            var max = numbers.Max();
+            var min = numbers.Min();
+            var avg = numbers.Average();
+            Console.WriteLine($"Sum :{sum}, Max :{max}, Min : {min} and Average : {avg}");
+        }
+
+        //aggregates with seed
+
+        static void Seed_Aggregations()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var result = numbers.Aggregate(10,(a,b) => a + b);   // 25
+            Console.WriteLine("Aggregates Sum with seed : {0}", result);
+
+            result = numbers.Aggregate((a,b)=> a * b);
+            Console.WriteLine("Just Aggregated product : {0}", result);
+        }
+
+        //Element Operators
+        static void Element_At()
+        {
+            string[] fruits = { "Apples", "Oranges", "Kiwi", "Papaya", "Banana" };
+
+            var result = fruits.ElementAt(3);  // papaya
+            Console.WriteLine(result);
+
+            //result = fruits.ElementAt(5);  // throws an exception
+
+            //to avoid exceptions
+
+            result = fruits.ElementAtOrDefault(5);
+            Console.WriteLine(result);
+        }
+
+        //positional operators
+        static void First_Operators()
+        {
+            string[] colors = { "Red", "Blue", "Green", "White", "Black", "Yellow" };
+
+            Console.WriteLine(colors.First());
+            Console.WriteLine(colors.Last());
+
+            string[] colors1 = { };
+            Console.WriteLine(colors1.FirstOrDefault());
+            Console.WriteLine(colors1.LastOrDefault());
+        }
+
+        //single operators
+        static void Single_Ops()
+        {
+            string[] names = { "Narendra Modi" };
+            string[] names2 = { "Donald Trump", "Nitanhu", "Obama" };
+            string[] empty = { };
+
+            Console.WriteLine(names.Single());
+
+            //Console.WriteLine(names2.Single());  // throws exception
+            // Console.WriteLine(names2.SingleOrDefault()); // throws exception
+            // Console.WriteLine(empty.Single());   // throws exception
+            Console.WriteLine(empty.SingleOrDefault());  // does not throw exception
+        }
+    }
+}
---
+++ ADO/XMLOperations/XMLOperations/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using System.Xml;
+
+namespace XMLOperations
+{
+    internal class Program
+    {
+        static DataSet ds = new DataSet("DS");
+        static void Main(string[] args)
+        {
+           // XmlWriter();
+            XmlReader();
+            Console.WriteLine("------------------------"); 
+            XmlSchemaWriter();
+            Console.WriteLine("------------------------");
+            XmlSchemaReader();
+            Console.Read();
+        }
+
+        static void ContentGeneration()
+        {
+            ds.Namespace = "StudentsSpace";
+            DataTable stdTable = new DataTable("Students");
+
+            stdTable.Columns.Add("Id",typeof(int));
+            stdTable.Columns.Add("Name",typeof(string));
+            stdTable.Columns.Add("Address", typeof(string));
+
+            DataRow dataRow = stdTable.NewRow();
+
+            dataRow["Id"] = 10;
+            dataRow["Name"] = "Mohan";
+            dataRow["Address"] = "Bangalore";
+
+            stdTable.Rows.Add(dataRow);
+
+            dataRow = stdTable.NewRow();
+
+            dataRow["Id"] = 12;
+            dataRow["Name"] = "Rohan";
+            dataRow["Address"] = "Mumbai";
+
+            stdTable.Rows.Add(dataRow);
+
+            dataRow = stdTable.NewRow();
+
+            dataRow["Id"] = 14;
+            dataRow["Name"] = "Sohan";
+            dataRow["Address"] = "Hyderabad";
+
+            stdTable.Rows.Add(dataRow);
+
+            dataRow = stdTable.NewRow();
+
+            dataRow["Id"] = 16;
+            dataRow["Name"] = "Jagan";
+            dataRow["Address"] = "Chennai";
+
+            stdTable.Rows.Add(dataRow);
+
+            //add the datatble to the dataset
+
+            ds.Tables.Add(stdTable);
+            ds.AcceptChanges();
+           
+        }
+        static void XmlWriter()
+        {
+            try
+            {
+                ContentGeneration();
+                StreamWriter sw = new StreamWriter("Student.xml");
+
+                ds.WriteXml(sw);
+                sw.Close();
+                Console.WriteLine("Xml file Created successfully...");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void XmlReader()
+        {
+            DataSet ds1 = new DataSet();
+            ds1.ReadXml("Student.xml");
+
+            foreach(DataTable dt in ds1.Tables)
+            {
+                Console.WriteLine(dt);
+
+                for(int i=0; i<dt.Columns.Count; i++)
+                {
+                    Console.Write("\t" + "\t" + dt.Columns[i].ColumnName);
+                }
+                Console.WriteLine();
+
+                foreach(DataRow row in dt.Rows)
+                {
+                    for(int j=0; j<dt.Columns.Count; j++)
+                    {
+                        Console.Write("\t" + "\t" + row[j]);
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("----------------------------------------------------------");
+            
+                foreach(var r in dt.AsEnumerable())
+                {
+                    for(int i = 0; i<dt.Columns.Count; i++)
+                    {
+                        Console.Write("\t" + "\t" + r[i]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        static void XmlSchemaWriter()
+        {
+            ContentGeneration();
+            ds.WriteXmlSchema("Studentschema");
+            Console.WriteLine("Xml schema Created successfully...");
+        }
+        static void XmlSchemaReader()
+        {
+            ds = new DataSet("StdSchema");
+
+            StreamReader sr = new StreamReader("Studentschema");
+
+            ds.ReadXmlSchema(sr);
+
+            //use foreach loop to iterate the dataset
+
+            //we can also use xmltextwriter
+            XmlTextWriter writer = new XmlTextWriter(Console.Out);
+            ds.WriteXmlSchema(writer);
+        }
+    }
+}
