e800ac5a598cd1e90d981bb127138472f0e6c47a
BanurekhaV <rekhasairam@hotmail.com>
2026-05-08 18:30:52 +0530

    JS

--- WebTechnologies/JavaScripts/ConsoleApp1/ConsoleApp1/FirstJS.js
+++ WebTechnologies/JavaScripts/ConsoleApp1/ConsoleApp1/FirstJS.js
@@ -1,65 +1,68 @@
 ﻿function varconcept() {
     var x = 5;
     console.log(x);
-    x = 10;  // reassigning possible
-    var x = 25;  // redeclaration is possible
-    var x = "AAA";  // redeclaration and reassigning possible
+    var x = 25;  //redeclaration is possible
+    var x = "AAA";  //reassigning is also possible
     console.log(x);
 }
-
 //let
 function Concepts() {
     let x = 25;
     if (x == 25) {
         let y = 10;
         console.log(y);
-        y = 100;  // reassigning or updating is possible
-        let y = 50;  //redeclartion is not possible
+        y = 100;  // reassigning or updation is possible
+        //   let y = 50; // redeclaration not possible
         console.log(y);
     }
 }
+function letconcepts() {
+   //  console.log(y1); // cannot access before initialization
+    let y1 = 6;  
+    console.log(y1);
+    
 
-function letConcepts() {
-    console.log(z);
-    let z = 5;
-    console.log(z);
+    //with objects
+    let { cname, age } = { cname: "Infinite Ltd.", age: 20 }
+    console.log(cname, age);
 }
-
-//with objects
-let { cname, age } = { cname: "Infinite Ltd.", age: 20 }
-console.log(cname, age);
 
 //const
 function constantconcepts() {
-    const c = 'a';
+    const c = 'a'; 
     const c1 = 10;
     console.log(c, c1);
-
+    //const with objects and arrays
     const cobj = { cname: "Infinite Ltd." };
     console.log(cobj.cname);
     cobj.cname = "New Infinite Ltd.";
     console.log(cobj.cname);
 
-    const arr = [2, 4, 6, 8];
-    arr.push(10);
-    console.log(arr);
+
+    const carr = [2, 4, 6, 8];
+    carr.push(10);
+    console.log(carr); //will display [2,4,6,8,10]
 }
 
 //global variables
-let pname = "Dhoni"; // global variable
+
+let pname = "Dhoni" // global variable
 function myFunction() {
-    let color = 'Yellow';  
-    console.log(typeof pname + " " + "Cricketers name is " + pname + " wears jersey " + color);
-    color = 'Blue'; 
+    let color = 'Red'; // considered global
+    console.log(typeof pname + " " + "Cricketer name is " + pname + " wears jersey of  " + color);
+    color = 'Blue'; //local variable
 }
 
-console.log(15 > 10);
-console.log(15 == "15");
-console.log(15 === "15");
+//operators
+
+console.log(15 > 10);  //true
+console.log(15 == "15"); //true
+console.log(15 === "15"); //false
+
 
 //ternary operator
 function ternary() {
     const age = 18;
-    const righttovote = age >= 18 ? "Yes" : "No";
+    const righttovote = age >= 18 ? "Yes Adult" : "No Minor";
     console.log(righttovote);
 }
