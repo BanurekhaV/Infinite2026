500646ccbb11f83c24680895c5d2b2f2f94dbedb
BanurekhaV <rekhasairam@hotmail.com>
2026-05-20 18:35:17 +0530

    exception

---
+++ ASP_WebForms/Exception_Prj/Exception_Prj/WebForm2.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Exception_Prj
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //option 1
+            //if(PreviousPage !=null)
+            //{
+            //    lblname.Text = ((TextBox)PreviousPage.FindControl("txtname")).Text;
+            //    lblemail.Text = ((TextBox)PreviousPage.FindControl("txtmail")).Text;
+            //}
+
+            //option 2
+            System.Collections.Specialized.NameValueCollection nvcprevious =
+                Request.Form;
+
+            lblname.Text = nvcprevious["txtname"];
+            lblemail.Text= nvcprevious["txtmail"];
+
+        }
+
+        protected void btnPostBack_Click(object sender, EventArgs e)
+        {
+            Response.Write("Hi I am Web Form 2");
+        }
+    }
+}
