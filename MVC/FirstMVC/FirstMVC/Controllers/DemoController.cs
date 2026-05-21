4f404266dcc4feafcc66503b5e92209e5eb8b42f
BanurekhaV <rekhasairam@hotmail.com>
2026-05-21 17:02:46 +0530

    MVC day 1

---
+++ MVC/FirstMVC/FirstMVC/Controllers/DemoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //various types of action results
+        //1. Normal method
+        public string NormalMethod()
+        {
+            return " Hi All !!";
+        }
+
+        //2. view result
+        public ViewResult ViewMethod()
+        {
+            return View();
+        }
+
+    }
+}
