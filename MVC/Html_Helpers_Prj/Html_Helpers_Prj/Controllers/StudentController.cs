ec13ef48a1bf54b4c1358e99e3b9db1a2bdf36f0
BanurekhaV <rekhasairam@hotmail.com>
2026-05-25 17:02:58 +0530

    mvc day 3

---
+++ MVC/Html_Helpers_Prj/Html_Helpers_Prj/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Html_Helpers_Prj.Models;
+
+namespace Html_Helpers_Prj.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //1. strongly typed helper
+        public ActionResult Strongly_Typed()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MVC/Html_Helpers_Prj/Html_Helpers_Prj/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Html_Helpers_Prj.Models
+{
+    public class Student
+    {
+        public int RNo { get; set; }
+        public string name {  get; set; }
+        public string Address {  get; set; }    
+    }
+}
