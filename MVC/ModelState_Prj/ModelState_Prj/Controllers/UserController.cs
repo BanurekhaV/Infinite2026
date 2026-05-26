1e0ab650e5770951f0eeaeb545b1e1eb165598d7
BanurekhaV <rekhasairam@hotmail.com>
2026-05-26 13:38:06 +0530

    mvc day 4

---
+++ MVC/ModelState_Prj/ModelState_Prj/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using ModelState_Prj.Models;
+
+namespace ModelState_Prj.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //1. If Validation Succeeds
+        public ActionResult UserStatus()
+        {
+            ViewBag.status = "Validation Successful";
+            return View();
+        }
+    }
+}
