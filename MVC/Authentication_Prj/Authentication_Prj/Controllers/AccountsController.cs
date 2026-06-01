58547954297b13b10a7cb903cfef66823c91b30b
BanurekhaV <rekhasairam@hotmail.com>
2026-06-01 16:51:57 +0530

    MVC day 7

---
+++ MVC/Authentication_Prj/Authentication_Prj/Controllers/AccountsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Authentication_Prj.Models;
+
+namespace Authentication_Prj.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User umodel)
+        {
+            using(Infinite_MVCSecurityEntities1 context = new Infinite_MVCSecurityEntities1())
+            {
+                bool IsValidUser = context.Users.Any(user => user.UserName.ToLower()==
+                umodel.UserName.ToLower() && user.UserPassword == umodel.UserPassword);
+
+                if (IsValidUser)
+                {
+                    FormsAuthentication.SetAuthCookie(umodel.UserName,false);
+                    return RedirectToAction("Index", "Employees");
+                }
+
+                ModelState.AddModelError("", "Invalid UserName or Password");
+                return View();
+            }
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(User umodel)
+        {
+            using (Infinite_MVCSecurityEntities1 context = new Infinite_MVCSecurityEntities1())
+            {
+                context.Users.Add(umodel);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ MVC/Authentication_Prj/Authentication_Prj/Models/UsersRoleProvider.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace Authentication_Prj.Models
+{
+    public class UsersRoleProvider : RoleProvider
+    {
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            using(Infinite_MVCSecurityEntities1 context = new Infinite_MVCSecurityEntities1())
+            {
+                var userRoles = (from user in context.Users
+                                 join rolemap in context.UserRolesMappings
+                                 on user.UserID equals rolemap.UserId
+                                 join role in context.RoleMasters
+                                 on rolemap.RoleId equals role.RoleID
+                                 where user.UserName == username
+                                 select role.RoleName).ToArray();
+                return userRoles;
+            }
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
