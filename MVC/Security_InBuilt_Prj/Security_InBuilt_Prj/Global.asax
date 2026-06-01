892293e157df074e00896fc3430ceef4f41d4712
BanurekhaV <rekhasairam@hotmail.com>
2026-06-01 13:51:36 +0530

    MVC day 7

---
+++ MVC/Security_InBuilt_Prj/Security_InBuilt_Prj/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Security_InBuilt_Prj.MvcApplication" Language="C#" %>
---
+++ MVC/Security_InBuilt_Prj/Security_InBuilt_Prj/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Security_InBuilt_Prj.Startup))]
+namespace Security_InBuilt_Prj
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
