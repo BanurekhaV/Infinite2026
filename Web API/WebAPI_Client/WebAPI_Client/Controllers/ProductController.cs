964af6ae06c57ca812f6e8a1eccf4f7c357ef527
BanurekhaV <rekhasairam@hotmail.com>
2026-06-02 17:12:56 +0530

    web api

---
+++ Web API/WebAPI_Client/WebAPI_Client/Controllers/ProductController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using WebAPI_Client.Models;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WebAPI_Client.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //action method to consume webapi service product/get
+        public ActionResult DisplayProducts()
+        {
+            IEnumerable<MVCProductModel> productlist = null;
+            using(var webclient = new HttpClient())
+            {
+                webclient.BaseAddress = new Uri("https://localhost:44343/api/");
+                var responsetalk = webclient.GetAsync("Product/getproducts");                              
+                responsetalk.Wait();
+
+                var result = responsetalk.Result;
+                if(result.IsSuccessStatusCode)
+                {
+                    var resultdata = result.Content.ReadAsStringAsync().Result;
+                    productlist = JsonConvert.DeserializeObject<List<MVCProductModel>>(resultdata);
+                }
+                else
+                {
+                    productlist = Enumerable.Empty<MVCProductModel>();
+                }
+                return View(productlist);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(MVCProductModel mvcprd)
+        {
+            using (var webclient = new HttpClient())
+            {
+                webclient.BaseAddress = new Uri("https://localhost:44343/api/");
+
+                var posttalk = webclient.PostAsJsonAsync<MVCProductModel>("Product/PostProduct", mvcprd);
+                posttalk.Wait();
+
+                var dataresult = posttalk.Result;
+
+                if(dataresult.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("DisplayProducts");
+                }
+
+                ModelState.AddModelError(String.Empty, "Product Insertion Failed..");
+                return View(mvcprd);
+            }
+        }
+    }
+}
---
+++ Web API/WebAPI_EF/WebAPI_EF/Controllers/ProductController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI_EF.Models;
+
+namespace WebAPI_EF.Controllers
+{
+    public class ProductController : ApiController
+    {
+        InfiniteDBEntities1 db = new InfiniteDBEntities1();
+
+        //Get : api/Product
+        public IQueryable<Product> GetProducts()
+        {
+            return db.Products;
+        }
+
+        public IHttpActionResult GetProductById(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        public IHttpActionResult Put(Product product)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+            try
+            {
+                db.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+
+            }
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        public IHttpActionResult PostProduct([FromBody]Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Validations Failed");               
+            }
+            db.Products.Add(new Product()
+            {
+                ProductId = product.ProductId,
+                ProductName = product.ProductName,
+                Price = product.Price,
+                QuantityAvailable = product.QuantityAvailable
+            });
+            db.SaveChanges();
+            return Ok("Success");
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            Product p = db.Products.Find(id);
+            if(p == null)
+            {
+                return NotFound();
+            }
+            db.Products.Remove(p);
+            db.SaveChanges();
+            return Ok(p);
+        }
+    }
+}
